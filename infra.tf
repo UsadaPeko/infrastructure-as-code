8c032bb7ba9107401553f4b3096f51a4448105fd
rhea <rhea@rheaui-MacBookPro.local>
2022-02-23 19:52:14 +0900

    fix: subnet id

--- infra.tf
+++ infra.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "iac-subnet-20220223" {
 // 4. Routing Table
 resource "aws_route_table" "iac-routing-table-20220223" {
   vpc_id = aws_vpc.iac-vpc-20220223.id
-  id = iac-subnet-20220223
+  id = aws_subnet.iac-subnet-20220223.id
 
   route {
     cidr_block = "0.0.0.0/0"
