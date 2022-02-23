560c52c8dd8fc4c3f5b243de7f66fa61e590e522
rhea <rhea@rheaui-MacBookPro.local>
2022-02-23 20:46:50 +0900

    fix: ec2 to backup, vpc renewal

--- infra.tf
+++ infra.tf
@@ -1,49 +1,49 @@
 // 1. VPC
-resource "aws_vpc" "iac-vpc-20220223" {
+resource "aws_vpc" "iac-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "iac-vpc-20220223"
+    Name = "iac-vpc"
   }
 }
 
 // 2. Internet Gateway
-resource "aws_internet_gateway" "iac-igw-20220223" {
-  vpc_id = aws_vpc.iac-vpc-20220223.id
+resource "aws_internet_gateway" "iac-igw" {
+  vpc_id = aws_vpc.iac-vpc.id
 
   tags = {
-    Name = "iac-igw-20220223"
+    Name = "iac-igw"
   }
 }
 
 // 3. Subnet
-resource "aws_subnet" "iac-subnet-20220223" {
-  vpc_id     = aws_vpc.iac-vpc-20220223.id
+resource "aws_subnet" "iac-subnet" {
+  vpc_id     = aws_vpc.iac-vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "iac-subnet-20220223"
+    Name = "iac-subnet"
   }
 }
 
 // 4. Routing Table
-resource "aws_route_table" "iac-routing-table-20220223" {
-  vpc_id = aws_vpc.iac-vpc-20220223.id
+resource "aws_route_table" "iac-routing-table" {
+  vpc_id = aws_vpc.iac-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.iac-igw-20220223.id
+    gateway_id = aws_internet_gateway.iac-igw.id
   }
 
   tags = {
-    Name = "iac-routing-table-20220223"
+    Name = "iac-routing-table"
   }
 }
 
 // 5. Routing Table Association (Subnet + Routing Table)
-resource "aws_route_table_association" "iac-routing-table-association-20220223" {
-  subnet_id      = aws_subnet.iac-subnet-20220223.id
-  route_table_id = aws_route_table.iac-routing-table-20220223.id
+resource "aws_route_table_association" "iac-routing-table-association" {
+  subnet_id      = aws_subnet.iac-subnet.id
+  route_table_id = aws_route_table.iac-routing-table.id
 }
 
 // 6. Route53
