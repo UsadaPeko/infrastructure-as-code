a073b43135191256b0f48a304d9991cfd9d33893
rhea-so <jeonghyeon.rhea@gmail.com>
2022-02-24 20:07:46 +0900

    fix: subnet cidr_block

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "iac-igw" {
 // 3. Subnet
 resource "aws_subnet" "iac-subnet-1" {
   vpc_id     = aws_vpc.iac-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "ap-northeast-2a"
 
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnet" "iac-subnet-1" {
 }
 resource "aws_subnet" "iac-subnet-2" {
   vpc_id     = aws_vpc.iac-vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.4.0/24"
   availability_zone = "ap-northeast-2c"
 
   tags = {
