b21cd55affdc3a940204104a4c807ee98bdbaa1f
rhea-so <jeonghyeon.rhea@gmail.com>
2022-02-24 20:26:24 +0900

    fix: portMappings - hostPort to 8080

--- memo.tf
+++ memo.tf
@@ -149,7 +149,7 @@ resource "aws_ecs_task_definition" "memo-ecs-task-definition" {
       portMappings = [
         {
           containerPort = 8080
-          hostPort      = 80
+          hostPort      = 8080
         }
       ]
     }
