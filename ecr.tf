3533c643f269a3155e4e4079714a3ec504db69d8
rhea-so <jeonghyeon.rhea@gmail.com>
2022-02-24 19:07:39 +0900

    add: Memo ECS Cluster

--- ecr.tf
+++ ecr.tf
@@ -6,13 +6,3 @@ resource "aws_ecr_repository" "iac-ecr" {
     scan_on_push = true
   }
 }
-
-// Memo Service를 ECS에 배포하기 위해 ECR을 만듦 - JeongHyeon Kim 2022.02.23
-resource "aws_ecr_repository" "memo-ecr" {
-  name                 = "memo-ecr"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
