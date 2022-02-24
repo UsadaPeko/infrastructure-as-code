cbf6f4d936b23f0bc55b74e2684e47f0c2b248bf
rhea-so <jeonghyeon.rhea@gmail.com>
2022-02-24 21:20:32 +0900

    fix: target group name

--- memo.tf
+++ memo.tf
@@ -109,8 +109,8 @@ resource "aws_lb" "memo-alb" {
 }
 
 // 6. Target Group
-resource "aws_lb_target_group" "memo-alb-target-group" {
-  name     = "memo-alb-target-group"
+resource "aws_lb_target_group" "memo-alb-target-group-1" {
+  name     = "memo-alb-target-group-1"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.iac-vpc.id
@@ -139,7 +139,7 @@ resource "aws_lb_listener" "memo-alb-listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.memo-alb-target-group.arn
+    target_group_arn = aws_lb_target_group.memo-alb-target-group-1.arn
   }
 }
 
@@ -211,7 +211,7 @@ resource "aws_ecs_service" "memo-ecs-service" {
   }
   
   load_balancer {
-    target_group_arn = aws_lb_target_group.memo-alb-target-group.arn
+    target_group_arn = aws_lb_target_group.memo-alb-target-group-1.arn
     container_name   = "memo-ecs-container"
     container_port   = 8080
   }
