75d4908bdb78228c9e7f798f16056e68a580fd7b
rhea-so <jeonghyeon.rhea@gmail.com>
2022-02-24 20:34:53 +0900

    add: IAM

--- memo.tf
+++ memo.tf
@@ -71,7 +71,33 @@ resource "aws_security_group" "memo-security-group" {
   }
 }
 
-// 4. Application Load Balancer
+// 4. IAM
+data "aws_iam_policy_document" "ecs-task-execution-role" {
+  version = "2012-10-17"
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs-task-execution-role" {
+  name               = "ecs-staging-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs-task-execution-role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role" {
+  role       = aws_iam_role.ecs-task-execution-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+// 5. Application Load Balancer
 resource "aws_lb" "memo-alb" {
   name               = "memo-alb"
   internal           = false
@@ -86,7 +112,7 @@ resource "aws_lb" "memo-alb" {
   }
 }
 
-// 5. Target Group
+// 6. Target Group
 resource "aws_lb_target_group" "memo-alb-target-group" {
   name     = "memo-alb-target-group"
   port     = 8080
@@ -94,7 +120,7 @@ resource "aws_lb_target_group" "memo-alb-target-group" {
   vpc_id   = aws_vpc.iac-vpc.id
 }
 
-// 6. Application Load Balancer Listener
+// 7. Application Load Balancer Listener
 resource "aws_lb_listener" "memo-alb-listener" {
   load_balancer_arn = aws_lb.memo-alb.arn
   port              = "80"
@@ -106,7 +132,7 @@ resource "aws_lb_listener" "memo-alb-listener" {
   }
 }
 
-// 7. ECS Cluster
+// 8. ECS Cluster
 resource "aws_ecs_cluster" "memo-ecs-cluster" {
   name = "memo-ecs-cluster"
 
@@ -120,7 +146,7 @@ resource "aws_ecs_cluster" "memo-ecs-cluster" {
   }
 }
 
-// 8. ECS Cluster Capacity Providers
+// 9. ECS Cluster Capacity Providers
 resource "aws_ecs_cluster_capacity_providers" "memo-ecs-cluster-capacity-providers" {
   cluster_name = aws_ecs_cluster.memo-ecs-cluster.name
 
@@ -133,13 +159,14 @@ resource "aws_ecs_cluster_capacity_providers" "memo-ecs-cluster-capacity-provide
   }
 }
 
-// 9. ECS Task Definition
+// 10. ECS Task Definition
 resource "aws_ecs_task_definition" "memo-ecs-task-definition" {
   family = "memo-ecs-task-definition"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu       = 256
   memory    = 512
+  execution_role_arn = aws_iam_role.ecs-task-execution-role.arn
   container_definitions = jsonencode([
     {
       name      = "memo-ecs-container"
@@ -148,17 +175,11 @@ resource "aws_ecs_task_definition" "memo-ecs-task-definition" {
                       // 참고: https://dealicious-inc.github.io/2021/05/10/ecs-fargate-benchmark-03.html
       memory    = 512
       essential = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
     }
   ])
 }
 
-// 10. ECS Service
+// 11. ECS Service
 resource "aws_ecs_service" "memo-ecs-service" {
   name            = "memo-ecs-service"
   cluster         = aws_ecs_cluster.memo-ecs-cluster.id
