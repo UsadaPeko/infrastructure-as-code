5974738793841a04d70627ba526af32d8c2c34d7
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-13 23:42:03 +0900

    move: main.tf into Environment Folder

--- 00_Environment/01_Default/03_Production/main.tf
+++ 00_Environment/01_Default/03_Production/main.tf
@@ -1,12 +1,12 @@
 // All services in this repository are created here
 module "rhea-so" {
-  source = "./01_Common/01_Route53/01_Zone"
+  source = "../../../01_Common/01_Route53/01_Zone"
 
   name = "rhea-so.com"
 }
 
 module "atlantis-rhea-so" {
-  source = "./01_Common/01_Route53/02_Record"
+  source = "../../../01_Common/01_Route53/02_Record"
 
   zone_id = module.rhea-so.zone_id
   name = "atlantis.rhea-so.com"
