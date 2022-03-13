9823a694809c7947cacbe998d1c6790d9434809d
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 02:40:41 +0900

    add: environment

---
+++ modules/environment/production/main.tf
@@ -0,0 +1,27 @@
+# module "rhea-so" {
+#   source = "../../01_Common/01_Route53/01_Zone"
+
+#   name = "rhea-so.com"
+# }
+
+# module "atlantis-rhea-so" {
+#   source = "../../01_Common/01_Route53/02_Record"
+
+#   zone_id = module.rhea-so.zone_id
+#   name = "atlantis.rhea-so.com"
+#   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
+# }
+
+# module "google-rhea-so" {
+#   source = "../../01_Common/01_Route53/02_Record"
+
+#   zone_id = module.rhea-so.zone_id
+#   name = "google.rhea-so.com"
+#   target = "172.217.31.174" # Google Test
+# }
+
+module "vpc" {
+  source = "../../aws/vpc"
+
+  name = "test"
+}
