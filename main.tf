709b7b1a38ab2f36887e8a2c79af5e715bb962d8
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 03:06:34 +0900

    fix: Route53

--- main.tf
+++ main.tf
@@ -1,12 +1,26 @@
 # All services in this repository are created here
+# Global Route53 Domain
+module "rhea-so" {
+  source = "./modules/aws/route53/zone"
+
+  name = "rhea-so.com"
+}
+
+# Environments
 module "production" {
 	source = "./modules/environment/production"
+
+	route53_zone_id = module.rhea-so.zone_id
 }
 
 module "staging" {
 	source = "./modules/environment/staging"
+
+	route53_zone_id = module.rhea-so.zone_id
 }
 
 module "dev" {
 	source = "./modules/environment/dev"
+
+	route53_zone_id = module.rhea-so.zone_id
 }
--- modules/environment/production/main.tf
+++ modules/environment/production/main.tf
@@ -1,13 +1,7 @@
-module "rhea-so" {
-  source = "../../aws/route53/zone"
-
-  name = "rhea-so.com"
-}
-
 module "atlantis-rhea-so" {
   source = "../../aws/route53/record"
 
-  zone_id = module.rhea-so.zone_id
+  zone_id = var.route53_zone_id
   name = "atlantis.rhea-so.com"
   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
 }
