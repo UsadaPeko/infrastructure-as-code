3561add8a13c317746bf14bc0c01f970ae475475
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-13 23:26:17 +0900

    test: Module

---
+++ main.tf
@@ -0,0 +1,14 @@
+// All services in this repository are created here
+module "rhea-so" {
+  source = "./01_Common/01_Route53/01_Zone"
+
+  name = "rhea-so.com"
+}
+
+module "atlantis-rhea-so" {
+  source = "./01_Common/01_Route53/02_Record"
+
+  zone_id = module.rhea-so.zone_id
+  name = "atlantis.rhea-so.com"
+  target = "52.78.144.248"
+}
