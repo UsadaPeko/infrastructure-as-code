ec85ce034bafd056022b7e3b87b6ea67bbed4065
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 00:32:26 +0900

    add: Google

--- 00_Environment/03_Production/main.tf
+++ 00_Environment/03_Production/main.tf
@@ -13,7 +13,7 @@ module "atlantis-rhea-so" {
   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
 }
 
-module "atlantis-rhea-so" {
+module "google-rhea-so" {
   source = "../../01_Common/01_Route53/02_Record"
 
   zone_id = module.rhea-so.zone_id
