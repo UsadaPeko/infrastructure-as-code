cb3eaf5be34720c9761fa0aa7324d134183e711e
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 03:19:10 +0900

    test: api gateway

---
+++ modules/README.md
@@ -0,0 +1,7 @@
+# ! Alert
+
+`atlantis.yaml`을 만들고, 내부에 `projects`를 설정해주지 않는 이상
+
+`modules` 폴더 외부에 module을 만드는 경우, module이 root directory로 잡혀서 (sub workspace) 원하는대로 IaC를 구성하지 못하고, 꼬이는 문제가 발생함
+
+항상 module은 `modules` 폴더 내부에 만들어두도록 할 것 !!
--- modules/environment/production/main.tf
+++ modules/environment/production/main.tf
@@ -6,8 +6,12 @@ module "atlantis-rhea-so" {
   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
 }
 
-module "production-vpc" {
-  source = "../../aws/vpc"
-
-  name = "production-vpc"
+module "api-gateway" {
+  source = "../../aws/api-gateway"
 }
+
+# module "production-vpc" {
+#   source = "../../aws/vpc"
+
+#   name = "production-vpc"
+# }
