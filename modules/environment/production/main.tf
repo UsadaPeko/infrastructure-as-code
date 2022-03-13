3a1a37680e9076c1ab15dc80add450be833135a7
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 03:36:05 +0900

    add: Production VPC

--- modules/environment/production/main.tf
+++ modules/environment/production/main.tf
@@ -6,8 +6,8 @@ module "atlantis-rhea-so" {
   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
 }
 
-# module "production-vpc" {
-#   source = "../../aws/vpc"
+module "production-vpc" {
+  source = "../../aws/vpc"
 
-#   name = "production-vpc"
-# }
+  name = "production-vpc"
+}
