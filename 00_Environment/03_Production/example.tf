e350c39a33606f63b0d7391445f500cf850898ab
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 01:26:36 +0900

    delete: exmaple

--- 00_Environment/03_Production/example.tf
+++ 00_Environment/03_Production/example.tf
@@ -1,7 +1,7 @@
-module "test-rhea-so" {
-  source = "../../01_Common/01_Route53/02_Record"
+# module "test-rhea-so" {
+#   source = "../../01_Common/01_Route53/02_Record"
 
-  zone_id = module.rhea-so.zone_id
-  name = "test.rhea-so.com"
-  target = "172.217.31.174" # Google Test
-}
+#   zone_id = module.rhea-so.zone_id
+#   name = "test.rhea-so.com"
+#   target = "172.217.31.174" # Google Test
+# }
