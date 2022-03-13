f63ce275a3fe4e92e34d9f3cc35744cf9a486b97
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 03:12:49 +0900

    delete: atlantis

--- modules/environment/production/main.tf
+++ modules/environment/production/main.tf
@@ -1,10 +1,10 @@
-# module "atlantis-rhea-so" {
-#   source = "../../aws/route53/record"
+module "atlantis-rhea-so" {
+  source = "../../aws/route53/record"
 
-#   zone_id = var.route53_zone_id
-#   name = "atlantis.rhea-so.com"
-#   target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
-# }
+  zone_id = var.route53_zone_id
+  name = "atlantis.rhea-so.com"
+  target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
+}
 
 module "production-vpc" {
   source = "../../aws/vpc"
