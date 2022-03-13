1e28e963e630ecf4fdd74150a15bfffc57ecd204
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 00:58:31 +0900

    test: example route53

--- 00_Environment/03_Production/main.tf
+++ 00_Environment/03_Production/main.tf
@@ -1,5 +1,22 @@
 # All services in this repository are created here
-resource "aws_route53_zone" "rhea-so" {
+module "rhea-so" {
+  source = "../../01_Common/01_Route53/01_Zone"
+
   name = "rhea-so.com"
-  force_destroy = true
+}
+
+module "atlantis-rhea-so" {
+  source = "../../01_Common/01_Route53/02_Record"
+
+  zone_id = module.rhea-so.zone_id
+  name = "atlantis.rhea-so.com"
+  target = "52.78.144.248" # Atlantis를 통해 만든 EC2가 아니어서, 직접 EC2 IP를 적어줌
+}
+
+module "google-rhea-so" {
+  source = "../../01_Common/01_Route53/02_Record"
+
+  zone_id = module.rhea-so.zone_id
+  name = "google.rhea-so.com"
+  target = "172.217.31.174" # Google Test
 }
