36fcad4a6e36ce46f394d87496dbf547a297032d
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-13 16:29:10 +0900

    test: api gateway address

--- Example/api_gateway.tf
+++ Example/api_gateway.tf
@@ -1,33 +1,44 @@
-resource "aws_api_gateway_rest_api" "api" {
-  name = "api-gateway"
+// 1. API Gateway
+resource "aws_api_gateway_rest_api" "iac-api-gateway" {
+  name = "iac-api-gateway"
   description = "Proxy to handle requests to our API"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  path_part   = "{proxy+}"
+// 2. Set up address
+# /google
+resource "aws_api_gateway_resource" "iac-api-gateway-resource-google" {
+  rest_api_id = "${aws_api_gateway_rest_api.iac-api-gateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.iac-api-gateway.root_resource_id}"
+  path_part   = "google"
 }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "ANY"
+# /google/enter
+resource "aws_api_gateway_resource" "iac-api-gateway-resource-google-enter" {
+  rest_api_id = "${aws_api_gateway_rest_api.iac-api-gateway.id}"
+  parent_id   = "${aws_api_gateway_resource.iac-api-gateway-resource-google.id}"
+  path_part   = "enter"
+}
+
+# /google/enter is GET
+resource "aws_api_gateway_method" "iac-api-gateway-method-google-enter" {
+  rest_api_id   = "${aws_api_gateway_rest_api.iac-api-gateway.id}"
+  resource_id   = "${aws_api_gateway_resource.iac-api-gateway-resource-google-enter.id}"
+  http_method   = "GET"
   authorization = "NONE"
-  request_parameters = {
-    "method.request.path.proxy" = true
-  }
 }
 
+# /google/enter is proxy into google.com
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_resource.resource.id}"
-  http_method = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "ANY"
+  rest_api_id = "${aws_api_gateway_rest_api.iac-api-gateway.id}"
+  resource_id = "${aws_api_gateway_resource.iac-api-gateway-resource-google-enter.id}"
+  http_method = "${aws_api_gateway_method.iac-api-gateway-method-google-enter.http_method}"
+  integration_http_method = "GET" # GET으로 Proxy
   type                    = "HTTP_PROXY"
-  uri                     = "http://google.com/{proxy}"
- 
-  request_parameters =  {
-    "integration.request.path.proxy" = "method.request.path.proxy"
-  }
+  uri                     = "http://google.com"
+}
+
+// Output
+output "iac-api-gateway" {
+  value       = "https://${aws_api_gateway_rest_api.iac-api-gateway.id}.execute-api.ap-northeast-2.amazonaws.com"
+  description = "Infrastructure as Code - api_gateway.tf"
 }
