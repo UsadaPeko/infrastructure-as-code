aaa9adb11a931cf60afb7144002901da6d9edaa8
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-14 00:54:46 +0900

    delete: atlantis route53

--- 00_Environment/03_Production/main.tf
+++ 00_Environment/03_Production/main.tf
@@ -3,11 +3,3 @@ resource "aws_route53_zone" "rhea-so" {
   name = "rhea-so.com"
   force_destroy = true
 }
-
-resource "aws_route53_record" "atlantis-rhea-so" {
-  zone_id = aws_route53_zone.rhea-so.zone_id
-  name = "atlantis.rhea-so.com"
-  type = "A"
-  ttl = "300"
-  records = ["52.78.144.248"]
-}
