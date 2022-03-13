79f834d8294886a1b6700be94a695f53ad165e3c
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-13 17:19:58 +0900

    add: Test VPC

--- Example/main.tf
+++ Example/main.tf
@@ -1,3 +1,65 @@
+// 1. VPC
+resource "aws_vpc" "iac-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "iac-vpc"
+  }
+}
+
+// 2. Internet Gateway
+resource "aws_internet_gateway" "iac-igw" {
+  vpc_id = aws_vpc.iac-vpc.id
+
+  tags = {
+    Name = "iac-igw"
+  }
+}
+
+// 3. Subnet
+resource "aws_subnet" "iac-subnet-1" {
+  vpc_id     = aws_vpc.iac-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "iac-subnet-1"
+  }
+}
+resource "aws_subnet" "iac-subnet-2" {
+  vpc_id     = aws_vpc.iac-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "iac-subnet-2"
+  }
+}
+
+// 4. Routing Table
+resource "aws_route_table" "iac-routing-table" {
+  vpc_id = aws_vpc.iac-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.iac-igw.id
+  }
+
+  tags = {
+    Name = "iac-routing-table"
+  }
+}
+
+// 5. Routing Table Association (Subnet + Routing Table)
+resource "aws_route_table_association" "iac-routing-table-association-1" {
+  subnet_id      = aws_subnet.iac-subnet-1.id
+  route_table_id = aws_route_table.iac-routing-table.id
+}
+resource "aws_route_table_association" "iac-routing-table-association-2" {
+  subnet_id      = aws_subnet.iac-subnet-2.id
+  route_table_id = aws_route_table.iac-routing-table.id
+}
+
 // 1. DB Subnet Group
 // 어떤 VPC - Subnet에 RDS를 만들 것인가?
 resource "aws_db_subnet_group" "iac-db-subnet-group" {
@@ -36,7 +98,6 @@ resource "aws_rds_cluster_instance" "iac-rds-cluster-instance-1" {
   engine_version     = aws_rds_cluster.iac-rds-cluster.engine_version
   db_subnet_group_name = aws_db_subnet_group.iac-db-subnet-group.name
 }
-
 resource "aws_rds_cluster_instance" "iac-rds-cluster-instance-2" {
   apply_immediately  = true
   publicly_accessible = true
