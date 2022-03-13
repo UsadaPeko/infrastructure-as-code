71e91585149e39e1dacf8b048360da6a7c322322
rhea-so <jeonghyeon.rhea@gmail.com>
2022-03-13 16:29:44 +0900

    fix: integration name

--- Example/api_gateway.tf
+++ Example/api_gateway.tf
@@ -28,7 +28,7 @@ resource "aws_api_gateway_method" "iac-api-gateway-method-google-enter" {
 }
 
 # /google/enter is proxy into google.com
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "iac-api-gateway-integration" {
   rest_api_id = "${aws_api_gateway_rest_api.iac-api-gateway.id}"
   resource_id = "${aws_api_gateway_resource.iac-api-gateway-resource-google-enter.id}"
   http_method = "${aws_api_gateway_method.iac-api-gateway-method-google-enter.http_method}"
